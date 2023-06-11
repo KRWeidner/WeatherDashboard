39f7f65d5112b1614bd4bcd82a0f07a00b01b6c9
KatyW <ktabe13@gmail.com>
2023-06-11 17:07:24 -0500

    add comments and tweaks

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -17,7 +17,7 @@ var date = today.format('MM/DD/YYYY');
 var cities = [];
 var citiesArray = [];
 
-//create buttons for every city in local storage upon initial load
+//create buttons for every city in local storage upon initial load and set unique ids
 var savedCities = localStorage.getItem('cities');
 if (savedCities != null) {
     citiesArray = JSON.parse(savedCities);
@@ -25,11 +25,22 @@ if (savedCities != null) {
     citiesArray.forEach(item => {
         var newButton = document.createElement("button");
         newButton.textContent = item.toUpperCase();
-        newButton.setAttribute("id",item+"Button");
+        newButton.setAttribute("id",item+"_Button");
         pastCitiesBtnSection.append(newButton);
+
+       // newButton.addEventListener('click', searchPastHistory(item));
     });
 }
 
+var searchPastHistory = function (city) {
+    console.log(city);
+    getWeather(city);
+    getForecast(city);
+    resultsBody.setAttribute("style","display:flex")
+    forecastSection.setAttribute("style","display:flex");
+}
+
+//gets entered city by user, ,if it exists, we call getWeather and getForecast functions and set results body to display in the UI
 var formSubmitHandler = function (event) {
     event.preventDefault();
     var city = searchCityInput.value.trim();
@@ -43,6 +54,7 @@ var formSubmitHandler = function (event) {
     }
 };
 
+//makes API call for current weather report. We are searching by the passed input city and converting results to imperial units
 var getWeather = function (city) {
     var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey;
 
@@ -50,7 +62,7 @@ var getWeather = function (city) {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
+                    //setting all the elements: temp, icon, wind, humidity
                     cityNameDate.textContent=city.toUpperCase()+" "+ date;
                     weatherIcon.setAttribute("src","https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png");
                     cityTemp.textContent="Temp: "+data.main.temp+"°F";
@@ -67,8 +79,9 @@ var getWeather = function (city) {
         });
 };
 
+//calling API for 5 day forecast. Again searching by input city and returning imperial units
 var getForecast = function (city) {
-    var apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&cnt=35&appid=' + apiKey;
+    var apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + apiKey;
 
     fetch(apiUrl)
         .then(function (response) {
@@ -76,14 +89,14 @@ var getForecast = function (city) {
                 response.json().then(function (data) {
                     var fiveDayForcast =[];
                     //gettting forecasts for the next 5 days from the noon reading for a mid day predication
-                    fiveDayForcast.push(data.list[2]);
-                    fiveDayForcast.push(data.list[10]);
-                    fiveDayForcast.push(data.list[18]);
-                    fiveDayForcast.push(data.list[26]);
-                    fiveDayForcast.push(data.list[34]);
-                    console.log(fiveDayForcast);
+                    fiveDayForcast.push(data.list[4]);
+                    fiveDayForcast.push(data.list[12]);
+                    fiveDayForcast.push(data.list[20]);
+                    fiveDayForcast.push(data.list[28]);
+                    fiveDayForcast.push(data.list[36]);
 
                     var dayCount =1;
+                    //for each day forecast, create a new div with 4 new elements for temp, icon, wind, humidity and append it to preexisting class forecastDays
                     fiveDayForcast.forEach(item => {
                         var newDiv = document.createElement("div");
                         newDiv.setAttribute("id","forecast"+dayCount);
@@ -116,12 +129,15 @@ var getForecast = function (city) {
         });
 }
 
+//adding a button to list of passed searched city buttons upon finished search of that city
 var addHistoryButton = function(passCity){
     var city = passCity.toUpperCase();
+    //get localstorage
     var savedCities = localStorage.getItem('cities');
     if (savedCities != null) {
         citiesArray = JSON.parse(savedCities);
     }
+    //don't add city to local storage if it already is in the list
     if(!citiesArray.includes(city))
     {
         citiesArray.push(city);
@@ -130,7 +146,32 @@ var addHistoryButton = function(passCity){
         pastCitiesBtnSection.append(newButton);
         newButton.setAttribute("id",city+"Button");
     }
+    //set local storage
     localStorage.setItem("cities", JSON.stringify(citiesArray));
 }
 
+//form event listener
 formBody.addEventListener('submit', formSubmitHandler);
+
+//console.log(pastCitiesBtnSection.children);
+// console.log(pastCitiesBtnSection.childElementCount);
+
+//  for(var i=0; i< pastCitiesBtnSection.childElementCount;i++)
+//  {
+    
+//      console.log(pastCitiesBtnSection.children[i]);
+//      var str = pastCitiesBtnSection.children[i].id;
+//      var mySubString = str.substring(
+//         str.indexOf("#") , 
+//         str.lastIndexOf("_")
+//     );
+//      console.log(str + " HERE");
+//      document.body.addEventListener('click', function (event) {
+//         if(event.target.id == str)
+//         {
+//             searchPastHistory(mySubString);
+//         }
+//      });
+     
+//      //pastCitiesBtnSection.children[i].addEventListener('click',searchPastHistory(str));
+//  }
