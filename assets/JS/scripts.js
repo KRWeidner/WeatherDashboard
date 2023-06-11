d1efa862d1500be845c085f4b682c92236ba5aba
KatyW <ktabe13@gmail.com>
2023-06-10 21:52:38 -0500

    adding weather icon

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -8,11 +8,26 @@ var searchCityInput = document.querySelector('#cityInput');
 var forecastSection = document.querySelector('.forecastSection');
 var formBody = document.querySelector('#form-body');
 var resultsBody = document.querySelector('.resultsBody');
+var weatherIcon = document.querySelector('#weatherIcon');
 
 var apiKey = "214431e28055374049b77802959fa948";
 var today = dayjs();
 var date = today.format('MM/DD/YYYY');
 var cities = [];
+var citiesArray = [];
+
+//create buttons for every city in local storage upon initial load
+var savedCities = localStorage.getItem('cities');
+if (savedCities != null) {
+    citiesArray = JSON.parse(savedCities);
+
+    citiesArray.forEach(item => {
+        var newButton = document.createElement("button");
+        newButton.textContent = item.toUpperCase();
+        newButton.setAttribute("id",item+"Button");
+        pastCitiesBtnSection.append(newButton);
+    });
+}
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -34,6 +49,7 @@ var getWeather = function (city) {
                 response.json().then(function (data) {
                     console.log(data);
                     cityNameDate.textContent=city.toUpperCase()+" "+ date;
+                    weatherIcon.setAttribute("src","https://openweathermap.org/img/wn/10d@2x.png");
                     cityTemp.textContent="Temp: "+data.main.temp+"°F";
                     cityHumidity.textContent="Humidity: "+data.main.humidity+"%";
                     cityWind.textContent="Wind: "+data.wind.speed+" MPH";
@@ -48,17 +64,21 @@ var getWeather = function (city) {
         });
 };
 
-var addHistoryButton = function(city){
-    var citiesArray = [];
+var addHistoryButton = function(passCity){
+    var city = passCity.toUpperCase();
     var savedCities = localStorage.getItem('cities');
     if (savedCities != null) {
         citiesArray = JSON.parse(savedCities);
     }
-    citiesArray.push(city);
+    if(!citiesArray.includes(city))
+    {
+        citiesArray.push(city);
+        var newButton = document.createElement("button");
+        newButton.textContent = city;
+        pastCitiesBtnSection.append(newButton);
+        newButton.setAttribute("id",city+"Button");
+    }
     localStorage.setItem("cities", JSON.stringify(citiesArray));
-    var newButton = document.createElement("button");
-    newButton.textContent = city.toUpperCase();
-    pastCitiesBtnSection.append(newButton);
 }
 
 formBody.addEventListener('submit', formSubmitHandler);
